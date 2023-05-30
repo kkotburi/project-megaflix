98135d5f721d22106e91077094df2e5acffe64cf
hyungjungson <thsgudwjd@nate.com>
2023-05-30 21:49:04 +0900

    작업완료

--- js/movies.js
+++ js/movies.js
@@ -5,21 +5,18 @@ const contain = document.querySelector(".contain");
 
 //API 서버에서 데이터 가져오는 함수
 function fetchMovie(page){
-	// const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=ko-KR&page=${page}`;
 	const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`;
 
 	fetch(url)
 		.then(res => res.json())
 		.then(function(res){
-			//console.log(res)
-			//console.log(res.results[0].id)
 			const movies = res.results;
 			movies.map(function(movie){
-				//console.log(movie.title);
+				console.log(movie.id)
 				const div = document.createElement('div');
 				div.classList.add('list');
 				const output = `
-																	<div class="post-entry-alt" onclick="openView()">
+																	<div class="post-entry-alt" onclick="openView(${movie.id})">
 																		<a class="img-link"><img src="${base_url + movie.poster_path}" alt="Image" class="img-fluid"></a>
 																		<div class="excerpt">
 																			<h2><a class="tit-movie">${movie.title}</a></h2>
@@ -32,28 +29,19 @@ function fetchMovie(page){
 																		<p><a href="#" class="read-more">개봉일: ${movie.release_date}</a></p>
 																		</div>
 																	</div>
-														
 																`;
 				div.innerHTML = output;
 				contain.appendChild(div);
-				// 영화의 아이디를 div 에 넣어 줌 - 나중에 이것을 get 해서 활용할 것임.
-				//div.classList.add("movie");
 				div.setAttribute("data-id", movie.id);
-				// div.setAttribute("onClick", "openView(this)");
-
 			});
 		})
 		.catch(erro => console.log(erro));
 }
 
 //썸네일 클릭시 실행 될 함수 event로 DOM 정보를 가져와서 필요한 데이터를 빼서 사용
-function openView(){
-	//alert(res.results[0].id)
-	// console.log(res)
-	//console.log(JSON.stringify(data));
+function openView(id){
+	alert(id)
 }
-
 
 //윈도우 로드시 기본으로 한번 함수 실행함.
 window.addEventListener('onLoad', fetchMovie());
-
