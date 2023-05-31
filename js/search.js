ee96de92f434b3ac88fe1a45a60b090d32e783dd
hyungjungson <thsgudwjd@nate.com>
2023-05-31 12:31:45 +0900

    변수명 수정

--- js/search.js
+++ js/search.js
@@ -3,10 +3,10 @@ function filter() {
 	let listInner = document.getElementsByClassName("list");
 
 	for (let i = 0; i < listInner.length; i++) {
-		city = listInner[i].getElementsByClassName("tit-movie");
-		country = listInner[i].getElementsByClassName("ov-movie");
-		if (city[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
-			country[0].innerHTML.toLowerCase().indexOf(search) != -1
+		titMovie = listInner[i].getElementsByClassName("tit-movie");
+		ovMovie = listInner[i].getElementsByClassName("ov-movie");
+		if (titMovie[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+			ovMovie[0].innerHTML.toLowerCase().indexOf(search) != -1
 		) {
 			listInner[i].style.display = "flex"
 		} else {
