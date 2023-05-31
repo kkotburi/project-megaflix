3d40f28231ed62d01dddb96304d2f0fa86c00766
hyungjungson <thsgudwjd@nate.com>
2023-05-31 12:11:42 +0900

    수정 작업 중

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// 글로벌 변수와 API 키에 대한 config.js 파일 설정
-export const config = {
-	api_key: '4e657bab9a1d4d7b73eb2631af49f6da',
-	api_base_url: 'https://api.themoviedb.org/3/',
-	image_base_url: 'https://image.tmdb.org/t/p/w1280'
-}
