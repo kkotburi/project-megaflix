aa4ce3ffe31cae777ac0f262509fa611f10e0a11
hyeyeong <kkotburiwillbloom@gmail.com>
2023-06-05 23:01:37 +0900

    module-based

--- js/modules/movies.js
+++ js/modules/movies.js
@@ -4,7 +4,7 @@ const base_url = "https://image.tmdb.org/t/p/w500/";
 const contain = document.querySelector(".contain");
 
 //API 서버에서 데이터 가져오는 함수
-function fetchMovie(page) {
+export const fetchMovie = (page) => {
   const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`;
 
   fetch(url)
@@ -41,13 +41,10 @@ function fetchMovie(page) {
         div.setAttribute("data-id", movie.id);
       });
     })
-    .catch((erro) => console.log(erro));
-}
+    .catch((err) => console.log(err));
+};
 
 //썸네일 클릭시 실행 될 함수 event로 DOM 정보를 가져와서 필요한 데이터를 빼서 사용
 // function openView(id) {
 //   alert(`영화 ID : ` + id);
 // }
-
-//윈도우 로드시 기본으로 한번 함수 실행함.
-window.addEventListener("onLoad", fetchMovie());
